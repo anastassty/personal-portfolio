2e1b1751881b2e813cca60010150c46c2df337c0
anastasty <ananastasiia.tykina@gmail.com>
2026-03-11 16:37:01 -0500

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Projects } from "@/sections/Projects";
 import { Experience } from "@/sections/Experience";
 import { Contact } from "@/sections/Contact";
 import { Footer } from "./layout/Footer";
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
   return( 
@@ -18,6 +20,7 @@ function App() {
         <Contact />
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
